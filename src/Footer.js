1809c619b7fb8659c0602c2d5ef8b58e14a554f4
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-11-22 18:17:09 +0200

    first and final version

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Kati Remo &copy; 2021</p>
+        </footer>
+    );
+};
+
+export default Footer;
